b9120fc06b576df769cee8b32ef4909523928e14
aadzhaan <adzhanilah@gmaiil.com>
2022-05-12 16:48:32 +0700

    change table structure

--- storage/framework/views/d64214ec2f5a9a0bc6009bd4ffaf82123f73b6c1.php
+++ storage/framework/views/d64214ec2f5a9a0bc6009bd4ffaf82123f73b6c1.php
@@ -28,53 +28,52 @@
                             All <code>Articles</code> that has been published on blog by the Admin of <code>Tekenens</code> are here.
 						</span>
 					</div>
-					<div class="card-block row">
-						<div class="col-sm-12 col-lg-12 col-xl-12">
-							<div class="table-responsive table-border-radius">
-								<table class="table table-hover table-striped ">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">No</th>
-                                            <th scope="col">Title</th>
-                                            <th scope="col">Published Date</th>
-                                            <th scope="col">Published By</th>
-                                            <th scope="col">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Mengapa Pemilihan Warna Penting?</td>
-                                            <td>February, 1st 2022</td>
-                                            <td>Natasha Aurelia</td>
-                                            <td>
-                                                <a class="btn btn-warning btn-pill btn-xs" href="" aria-label="Edit">
-                                                    <i data-feather="edit-2"></i>
-                                                </a>
-                                                <a class="btn btn-danger btn-pill btn-xs" href="" aria-label="Delete">
-                                                    <i data-feather="trash"></i>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Pecinta Anime wajib banget baca Manga ini!</td>
-                                            <td>January, 25th 2022</td>
-                                            <td>Rifky Asyari</td>
-                                            <td></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+					<div class="card-body">
+                        <div class="table-responsive">
+							<table class="row-border" id="example-style-7">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Title</th>
+                                        <th>Published Date</th>
+                                        <th>Published By</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Mengapa Pemilihan Warna Penting?</td>
+                                        <td>February, 1st 2022</td>
+                                        <td>Natasha Aurelia</td>
+                                        <td>
+                                            <a class="btn btn-warning btn-pill btn-xs" href="" aria-label="Edit">
+                                                <i data-feather="edit-2"></i>
+                                            </a>
+                                            <a class="btn btn-danger btn-pill btn-xs" href="" aria-label="Delete">
+                                                <i data-feather="trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Pecinta Anime wajib banget baca Manga ini!</td>
+                                        <td>January, 25th 2022</td>
+                                        <td>Rifky Asyari</td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
 					</div>
 				</div>
 			</div>
-
         </div>
 	</div>
 
 	<?php $__env->startPush('scripts'); ?>
+    <script src="<?php echo e(asset('assets/js/datatable/datatables/jquery.dataTables.min.js')); ?>"></script>
+    <script src="<?php echo e(asset('assets/js/datatable/datatables/datatable.custom.js')); ?>"></script>
 	<?php $__env->stopPush(); ?>
 
 <?php $__env->stopSection(); ?>
