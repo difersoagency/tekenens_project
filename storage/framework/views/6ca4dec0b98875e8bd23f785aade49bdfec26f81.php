15b705ed319dc9a9abb02de2d3415f3c65a453c7
wisnusunadi <barto7773@gmail.com>
2022-06-03 16:53:32 +0700

    Unfix Partner

---
+++ storage/framework/views/6ca4dec0b98875e8bd23f785aade49bdfec26f81.php
@@ -0,0 +1,21 @@
+<div class="container-fluid">
+    <div class="page-header">
+      <div class="row">
+        <div class="col-lg-6">
+          <?php echo e($breadcrumb_title ?? ''); ?>
+
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?php echo e(route('dashboard')); ?>">Dashboard</a></li>
+              <?php echo e($slot ?? ''); ?>
+
+          </ol>
+        </div>
+        <div class="col-lg-6">
+          <!-- Bookmark Start-->
+          
+          <!-- Bookmark Ends-->
+        </div>
+      </div>
+    </div>
+</div>
+<?php /**PATH C:\P\theme22\resources\views/components/breadcrumb.blade.php ENDPATH**/ ?>
